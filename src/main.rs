4959b43f40bcb07feec0171dd95e181d3617d8a3
Do Anh Tu <anhtu.do1998@gmail.com>
2022-02-16 05:08:53 +0000

    Refactor into classic game loop

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::WindowCanvas;
 use std::time::Duration;
+
+fn render(canvas: &mut WindowCanvas, color: Color){
+    canvas.set_draw_color(color);
+    canvas.clear();
+    // refresh, otherwise will not see anything
+    canvas.present();
+}
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem.window("Rust Game Tutorial", 800, 600)
         .position_centered()
         .build()
         .expect("Window build failed!");
 
     let mut canvas = window.into_canvas().build().expect("Canvas build failed!");
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // Handle event
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -33,7 +37,13 @@ fn main() -> Result<(), String> {
         }
         // The rest of the game loop goes here...
 
-        canvas.present();
+        // Update next color
+        i = (i + 1) % 255;
+
+        // Render
+        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+
+        // time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
     Ok(())
